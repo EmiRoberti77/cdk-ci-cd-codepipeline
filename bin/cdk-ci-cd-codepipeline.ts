68109f6d323d1f06b6154c5c657707c41ab775d8
EmiRoberti77 <emiliano.roberti@accenture.com>
2025-04-27 19:46:51 +0100

    app.synth

--- bin/cdk-ci-cd-codepipeline.ts
+++ bin/cdk-ci-cd-codepipeline.ts
@@ -4,3 +4,4 @@ import { CdkCiCdCodepipelineStack } from '../lib/cdk-ci-cd-codepipeline-stack';
 
 const app = new cdk.App();
 new CdkCiCdCodepipelineStack(app, 'CdkCiCdCodepipelineStack', {});
+app.synth();
--- lib/cdk-ci-cd-codepipeline-stack.ts
+++ lib/cdk-ci-cd-codepipeline-stack.ts
@@ -1,8 +1,24 @@
 import * as cdk from 'aws-cdk-lib';
+import {
+  CodePipeline,
+  CodePipelineSource,
+  ShellStep,
+} from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
 
 export class CdkCiCdCodepipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    new CodePipeline(this, 'cdk-emi-pipeline', {
+      pipelineName: 'cdk-emi-pipeline',
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.gitHub(
+          'EmiRoberti77/cdk-ci-cd-codepipeline',
+          'main'
+        ),
+        commands: ['npm ci', 'npx cdk synth'],
+      }),
+    });
   }
 }
