42e0a6e945d6af02659b8165da5d85f505611dd8
EmiRoberti77 <emiliano.roberti@accenture.com>
2025-04-27 19:26:22 +0100

    refresh project

--- lib/cdk-ci-cd-codepipeline-stack.ts
+++ lib/cdk-ci-cd-codepipeline-stack.ts
@@ -1,18 +1,8 @@
 import * as cdk from 'aws-cdk-lib';
-import * as s3 from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
-
-const bucketName = 'emi-cdk-ci-cd-codepipeline-bucket';
 
 export class CdkCiCdCodepipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    //create a new S3
-    new s3.Bucket(this, bucketName, {
-      versioned: true,
-      bucketName,
-    });
   }
 }
