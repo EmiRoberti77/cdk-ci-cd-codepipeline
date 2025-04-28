bfb62a21a0c55d6e991ec3f5199cc47f182374e2
EmiRoberti77 <emiliano.roberti@accenture.com>
2025-04-28 13:37:20 +0100

    refactor pipeline into a method

--- lib/cdk-ci-cd-codepipeline-stack.ts
+++ lib/cdk-ci-cd-codepipeline-stack.ts
@@ -8,30 +8,65 @@ import {
 import { Construct } from 'constructs';
 import { PipelineStage } from './pipelineStage';
 
+const githubPath = 'EmiRoberti77/cdk-ci-cd-codepipeline';
+
 export class CdkCiCdCodepipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const pipeline = new CodePipeline(this, 'cdk-emi-pipeline', {
-      pipelineName: 'cdk-emi-pipeline',
-      synth: new ShellStep('Synth', {
+    // 1️⃣ DEV Pipeline
+    this.buildPipeLine(
+      'dev',
+      githubPath,
+      'dev',
+      ['npm ci', 'npx cdk synth'],
+      'devEmiCdkPipeline',
+      ['npm ci', 'npm run test'],
+      'emiStageCdkPipeline',
+      'emiUnitTestPipeline',
+      'devSynthShell'
+    );
+    // 2️⃣ UAT Pipeline
+    const uatPipeline = new CodePipeline(this, 'UatPipeline', {
+      pipelineName: 'cdk-emi-uat-pipeline',
+      synth: new ShellStep('Synth-Uat', {
         input: CodePipelineSource.gitHub(
           'EmiRoberti77/cdk-ci-cd-codepipeline',
-          'main'
+          'uat' // UAT branch
         ),
         commands: ['npm ci', 'npx cdk synth'],
       }),
     });
+  }
 
-    const testStage = pipeline.addStage(
-      new PipelineStage(this, 'EmiCdkPipelineTest', {
-        stageName: 'test',
+  buildPipeLine(
+    stageName: string,
+    github: string,
+    branch: string,
+    buildCommands: string[],
+    pipelineName: string,
+    testCommands: string[],
+    pipelineStageName: string,
+    testPipeLineName: string,
+    shellStep: string
+  ) {
+    const devPipeline = new CodePipeline(this, pipelineName, {
+      pipelineName,
+      synth: new ShellStep(shellStep, {
+        input: CodePipelineSource.gitHub(github, branch),
+        commands: buildCommands,
+      }),
+    });
+
+    const devStage = devPipeline.addStage(
+      new PipelineStage(this, pipelineStageName, {
+        stageName,
       })
     );
 
-    testStage.addPre(
-      new CodeBuildStep('unit-test', {
-        commands: ['npm ci', 'npm run test'],
+    devStage.addPre(
+      new CodeBuildStep(testPipeLineName, {
+        commands: testCommands,
       })
     );
   }
